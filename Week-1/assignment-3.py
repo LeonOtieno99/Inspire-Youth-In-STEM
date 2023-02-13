92d8cd2d34d10ec46d94b1eee8e34303d0d2fd0a
LeonOtieno99 <leoncifer99@gmail.com>
2023-02-13 08:01:05 -0800

    First python code

---
+++ Week-1/assignment-3.py
@@ -0,0 +1,16 @@
+print("                x                   ")
+print("               xxx                  ")
+print("              xxxxxx                ")
+print("             xxxxxxxx               ")
+print("            xxxxxxxxxxx             ")
+print("           xxxxxxxxxxxxxx           ")
+
+
+print("                     *                 ")
+print("                   *****               ")
+print("                  ********             ")
+print("                 **********            ")
+print("               ***************         ")
+print("                 ***********           ")
+print("                    *****              ")
+print("                      *                ")
---
+++ Week-1/assignment-4.py
@@ -0,0 +1,24 @@
+company_name = "EQUITY  BANK"
+branch_name  = "CBD BRANCH"
+current_date = " 8th Feb 2023"
+f_name = "Leon"
+s_name = "Otieno"
+Acc_no = 16859912
+Acc_balance = 53000.12
+last_statement = "Equity Brings Togetherness"
+
+print("************************************************************")
+print("*                      ",company_name,"                      *")
+print("*                       ",branch_name,"                       *")
+print("*                     ",current_date,"                      *")
+print("*                                                          *")
+print("************************************************************")
+print("*                                                          *")
+print("*   NAME:              ",f_name,"",s_name,"                      *")
+print("*   ACCOUNT NO:        ",Acc_no,"                          *")
+print("*   ACCOUNT BALANCE:    Kshs.",Acc_balance,"                    *")
+print("*                                                          *")
+print("*             ",last_statement,"                 *")
+print("************************************************************")
+
+
