94e5663f0b520b762bb48bd9286667455b151bbe
LeonOtieno99 <leoncifer99@gmail.com>
2023-02-27 14:03:46 +0300

    OOP,FILE_IO

---
+++ Week-4/object-oriented-programming.py
@@ -0,0 +1,11 @@
+#class - a group of objects
+
+#objects - instance of the class with unique parameters
+#students - mark mercy
+#student
+#cars
+#abstraction
+#polymorphism- ability to take many forms
+#encapsulation
+#inheritence
+#object - an instance of the class
